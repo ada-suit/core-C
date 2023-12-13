81e6d71076269452e84ed773c279ce0d15915b7d
karsh <utkarsh@cyberfear.com>
2023-12-14 10:13:20 +1100

    ✨ setup program
    create all necessary directories and start systemd service for auto start

--- CONFIG/setup.sh
+++ CONFIG/setup.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 
+# Directory Structure
+mkdir ~/pulse
+cd ~/pulse
+mkdir logs src saves lib
+
+# Get Service file
+sudo curl -o /etc/systemd/system/wavelinkPulse.service https://raw.githubusercontent.com/karshPrime/wavelinkPulse/main/CONFIG/wavelinkPulse.service
+
+# Start and enable the service file
+sudo systemctl enable wavelinkPulse.service
+sudo systemctl start wavelinkPulse.service
