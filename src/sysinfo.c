0229dd83f0ea7954f4befeab6c3e939a414d50e5
karsh <utkarsh@cyberfear.com>
2023-12-20 12:11:52 +1100

    ✨ run command

--- src/sysinfo.c
+++ src/sysinfo.c
@@ -1,2 +1,24 @@
 #include "sysinfo.h"
 
+int run_command(const char *command, char *buffer, short buffer_size)
+{
+    FILE *cmd_output = popen(command, "r");
+    
+    if (cmd_output == NULL) {
+        perror("Error opening pipe");
+        return -1;
+    }
+
+    if (fgets(buffer, buffer_size, cmd_output) == NULL) {
+        perror("Error reading command output");
+        return -1;
+    }
+    
+    if (pclose(cmd_output) == -1) {
+        perror("Error closing pipe");
+        return -1;
+    }
+
+    return 0;
+}
+
