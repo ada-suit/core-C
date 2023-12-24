ac5430dbe26c049eef5c459896889d2d84b65148
karsh <utkarsh@cyberfear.com>
2023-12-25 09:09:16 +1100

    🐛 loop counter
    with the last approach, actions linked to a certain counter would trigger n number of times unless the counter's next updated.
    
    there were ways to prevent that using more variables and more logic, but that'd be on the cost of efficiency and code simplicity.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
 #include "main.h"
 
 int main() {
-    int counter = 0;
-    time_t current_time, start_time;
-    time(&start_time);
-
     struct gpiod_chip *chip;
     chip_init(&chip);
 
@@ -14,15 +10,17 @@ int main() {
     struct gpiod_line *led_power;
     line_init(&led_power, chip, "power stability", 26, 'o');
 
+    int counter = 0;
     int stop = 0;
     while (!stop) {
-        // trigger every 5mins
+        // trigger every 300 loops
         if (counter % 300 == 0) { 
-
             gpiod_line_set_value(led_power, power_stable());
         }
 
         counter_update(&counter, &start_time, &current_time);
+
+        counter++;
     }
 
     // Release resources
@@ -31,15 +29,4 @@ int main() {
     gpiod_chip_close(chip);
 
     return 0;
-}
-
-void counter_update(int *value, time_t *last_time, time_t *now_time) {
-    // Get the current time
-    time(now_time);
-
-    // Check if the current time has changed
-    if (*now_time != *last_time) {
-        *last_time = *now_time;
-        (*value)++;
-    }
 }
--- src/main.h
+++ src/main.h
@@ -4,12 +4,9 @@
 #include <stdio.h>
 #include <gpiod.h>
 #include <unistd.h>
-#include <time.h>
 
 #include "setup.h"
 #include "inform.h"
 #include "sysinfo.h"
-
-void counter_update(int *value, time_t *start_time, time_t *current_time);
 
 #endif
