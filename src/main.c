cbf6b6ad094bd40b25cfa396d054f843719eb955
karsh <utkarsh@cyberfear.com>
2023-12-26 10:07:26 +1100

    ✨ clearer comments

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 
 int main() 
 {
+    // setting up the counter
     struct Counter counter = {0, 1};
     time_t time_now, time_last;
     time(&time_last);
 
+    // initialising the chip
     struct gpiod_chip *chip;
     chip_init(&chip);
 
@@ -18,8 +20,8 @@ int main()
     struct gpiod_line *b_terminate;
     line_init(&b_terminate, chip, "terminate button", 24, 'i');
     
-    int stop = 0;
-    while (!stop) {
+    // main loop; runs forever unless requested to stop
+    while (1) {
         // trigger every 5 minutes
         if (counter.value % FIVE_MIN == 0 && counter.flag) {
             gpiod_line_set_value(led_power, power_stable());
@@ -31,19 +33,20 @@ int main()
             break;
         }
 
-        stop = counter.value == 20;
+        // update the counter
         counter_update(&time_now, &time_last, &counter);
     }
 
-    // Release resources
     gpiod_line_release(led_power);
     gpiod_line_release(buzzer);
+    // release resources
     gpiod_chip_close(chip);
 
     return 0;
 }
 
-// increment counter with time
+// increment counter with time (seconds)
+// todo: account delay with all sleep statements
 void counter_update(time_t *ntime, time_t *ltime, struct Counter *counter) 
 {
     time(ntime);
