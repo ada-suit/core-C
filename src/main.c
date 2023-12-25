6a64681dea676795dfb465037af7bd42e5043256
karsh <utkarsh@cyberfear.com>
2023-12-25 17:33:51 +1100

    ✨ terminate project
    added terminate button

--- src/main.c
+++ src/main.c
@@ -15,11 +15,20 @@ int main()
     struct gpiod_line *led_power;
     line_init(&led_power, chip, "power stability", 26, 'o');
 
+    struct gpiod_line *b_terminate;
+    line_init(&b_terminate, chip, "terminate button", 24, 'i');
+    
     int stop = 0;
     while (!stop) {
         // trigger every 5 minutes
         if (counter.value % FIVE_MIN == 0 && counter.flag) {
             gpiod_line_set_value(led_power, power_stable());
+        }
+
+        int v_terminate = gpiod_line_get_value(b_terminate);
+        if (v_terminate < 1) {
+            printf("%d\n", v_terminate);
+            break;
         }
 
         stop = counter.value == 20;
