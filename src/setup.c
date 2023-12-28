d6057da56b0d6f376883759512c143943fb2f931
karsh <utkarsh@cyberfear.com>
2023-12-28 21:14:13 +1100

    🐛 condition always true
    *chip was already dereferenced in the condition

--- src/setup.c
+++ src/setup.c
@@ -48,7 +48,7 @@ int component_total(int component)
 void chip_init(struct gpiod_chip **chip, bool *start)
 {
     *chip = gpiod_chip_open_by_name(GPIO_CHIP_NAME);
-    if (!(*chip)) {
+    if (*chip == NULL) {
         printe(1001, "GPIO chip", SEVERE);
         start = false;
     }
