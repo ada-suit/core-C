34355cac8e5b540d29a94307cf89ac9dfb22fa63
karsh <utkarsh@cyberfear.com>
2024-01-02 15:02:01 +1100

    ✨ button actions
    updated the call function with conditions for each button press. will populate the cases with functionalities in future

--- src/components/button.c
+++ src/components/button.c
@@ -27,13 +27,66 @@ void buttons_init(Button *buttons, UNIT_CHIP *chip, bool *start)
 }
 
 // trigger different actions depending on button pressed
-void button_call(short condition)
+void button_call(short condition, bool *shift)
 {
+    condition = (condition * 10) + *shift;
     switch (condition) {
-        case 10: // 10 = button 1 & toggle shift off
+        // ======== toggle shift ======== //
+        case 10: // shift off
+            *shift = true;
             break;
 
-        case 11:
+        case 11: // shift on
+            *shift = false;
+            break;
+        
+        // ========== button 1 ========== //
+        case 100: // shift off
+            break;
+
+        case 101: // shift on
+            break;
+
+        // ========== button 2 ========== //
+        case 1000: // shift off
+            break;
+
+        case 1001: // shift on
+            break;
+
+        // ========== button 3 ========== //
+        case 1100: // shift off
+            break;
+
+        case 1101: // shift on
+            break;
+
+        // ========== button 4 ========== //
+        case 10000: // shift off
+            break;
+
+        case 10001: // shift on
+            break;
+
+        // ========== button 5 ========== //
+        case 10100: // shift off
+            break;
+
+        case 10101: // shift on
+            break;
+
+        // ========== button 6 ========== //
+        case 11000: // shift off
+            break;
+
+        case 11001: // shift on
+            break;
+
+        // ========== button 7 ========== //
+        case 11100: // shift off
+            break;
+
+        case 11101: // shift on
             break;
         
         default:
@@ -45,7 +98,7 @@ void button_call(short condition)
 void buttons_update(Button *buttons, uint *counter, bool *shift)
 {
     int i = 1;
-    short condition = *shift;
+    short condition = 0;
 
     for (i = 1; i < BUTTONS_COUNT; i++) {
         if (buttons[i].sleep <= *counter) {
@@ -62,7 +115,15 @@ void buttons_update(Button *buttons, uint *counter, bool *shift)
         }
     }
 
-    button_call(condition);
+    /* condition digits = vvvt
+        v = redirect to Virtual button
+        t = Toggle shift
+    */
+
+    if (condition != 0) {
+        // dont call the function if no button is pressed
+        button_call(condition, shift);
+    }
 }
 
 // release resource
