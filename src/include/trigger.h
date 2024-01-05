fa25efe48380b20e094b40360b0ea3c62f58da8f
karsh <utkarsh@cyberfear.com>
2024-01-06 00:45:08 +1100

    🐛 fix led pointer
    fix arguments for the functions that requested pointer to leds array

--- src/include/trigger.h
+++ src/include/trigger.h
@@ -6,9 +6,9 @@
 void button_action(short status, UNIT_LINE *leds[], bool *shift, const uint count);
 void automation(UNIT_LINE *leds[], const uint *counter);
 
-void warn_indicate(UNIT_LINE *buzzer, UNIT_LINE *led, uint *counter);
-void notify_indicate(UNIT_LINE *buzzer, UNIT_LINE *led, uint *counter);
-void alarm_indicate(UNIT_LINE *buzzer, UNIT_LINE *led, uint *counter);
+void warn_indicate(UNIT_LINE *buzzer, UNIT_LINE *led[], uint *counter);
+void notify_indicate(UNIT_LINE *buzzer, UNIT_LINE *led[], uint *counter);
+void alarm_indicate(UNIT_LINE *buzzer, UNIT_LINE *led[], uint *counter);
 
 void counter_update(uint *counter);
 
--- src/trigger.c
+++ src/trigger.c
@@ -85,16 +85,17 @@ void automation(UNIT_LINE *leds[], const uint *counter)
 }
 
 
+void warn_indicate(UNIT_LINE *buzzer, UNIT_LINE *led[], uint *counter)
 {
     //
 }
 
-void notify_indicate(UNIT_LINE *buzzer, UNIT_LINE *led, uint *counter)
+void notify_indicate(UNIT_LINE *buzzer, UNIT_LINE *led[], uint *counter)
 {
     //
 }
 
-void alarm_indicate(UNIT_LINE *buzzer, UNIT_LINE *led, uint *counter)
+void alarm_indicate(UNIT_LINE *buzzer, UNIT_LINE *led[], uint *counter)
 {
     //
 }
